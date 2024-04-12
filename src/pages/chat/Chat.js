6408dd057d257dcd24c306e19ca07afe1a83b258
honorific <honorific.info@gmail.com>
2024-04-12 11:04:20 +0330

    chat page skeleton

---
+++ src/pages/chat/Chat.js
@@ -0,0 +1,12 @@
+import './chat.css'
+
+const Chat = () => {
+  return (
+    <div className='container'>
+      <div className='rightbar'></div>
+      <div className='main'></div>
+    </div>
+  )
+}
+
+export default Chat
