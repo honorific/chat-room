13d0b23af94cd43183d0f452c8b9e48214e32026
honorific <honorific.info@gmail.com>
2024-04-14 16:31:41 +0330

    styling the online user

--- src/components/onlineUser/OnlineUser.styles.js
+++ src/components/onlineUser/OnlineUser.styles.js
@@ -1,7 +1,26 @@
 import styled from 'styled-components'
 
-const StyledOnlineUser = styled.li `
+const StyledOnlineUser = styled.li`
+  margin-top: 5px;
+  font-size: 18px;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 5px;
+  transition: 0.3s ease-out;
+  & > div {
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+  }
+  &:hover {
+    background-color: aliceblue;
+  }
+  &.chatting::after {
+    content: '';
+    width: 5px;
+    height: 5px;
+    border-radius: 5px;
+  }
+`
 
-`;
-
-export default StyledOnlineUser;
+export default StyledOnlineUser
