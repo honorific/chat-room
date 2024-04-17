cccc0946d15836c4785959355df5ce863cb6bf08
honorific <honorific.info@gmail.com>
2024-04-17 14:07:40 +0330

    make Room and user component

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -1,36 +1,13 @@
-import OnlineUser from '../onlineUser/OnlineUser'
-import RoomTitle from '../roomTitle/RoomTitle'
+import RoomAndUsers from '../roomAndUsers/RoomAndUsers'
 import {StyledRightbar} from './Rightbar.styles'
 
 const Rightbar = () => {
   return (
     <StyledRightbar>
       <div className='content'>
-        <ul className='onlineUsers'>
-          <OnlineUser gender='male' username='ali' />
-          <OnlineUser gender='female' username='zahra' />
-          <OnlineUser gender='male' username='ali' chatting={{number: 3}} />
-          <OnlineUser gender='female' username='zahra' />
-          <OnlineUser gender='male' username='ali' />
-          <OnlineUser gender='female' username='zahra' />
-          <OnlineUser gender='male' username='ali' />
-          <OnlineUser gender='female' username='zahra' chatting />
-          <OnlineUser gender='male' username='ali' />
-          <OnlineUser gender='female' username='zahra' />
-        </ul>
-        <RoomTitle title='German speakers' />
-        <ul className='onlineUsers'>
-          <OnlineUser gender='male' username='ali' />
-          <OnlineUser gender='female' username='zahra' />
-          <OnlineUser gender='male' username='ali' chatting={{number: 3}} />
-          <OnlineUser gender='female' username='zahra' />
-          <OnlineUser gender='male' username='ali' />
-          <OnlineUser gender='female' username='zahra' />
-          <OnlineUser gender='male' username='ali' />
-          <OnlineUser gender='female' username='zahra' chatting />
-          <OnlineUser gender='male' username='ali' />
-          <OnlineUser gender='female' username='zahra' />
-        </ul>
+        <RoomAndUsers />
+        <RoomAndUsers otherRoom={true} roomTitle='German speakers' />
+        <RoomAndUsers otherRoom={true} roomTitle='Russian speakers' />
       </div>
     </StyledRightbar>
   )
---
+++ src/components/roomAndUsers/RoomAndUsers.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledRoomAndUsers = styled.div`
+  & .onlineUsers {
+    list-style-type: none;
+    margin: 0px;
+  }
+`
