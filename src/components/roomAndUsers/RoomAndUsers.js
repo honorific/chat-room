70c20c1f63bfc316ec81e29798eac032c90c8df6
honorific <honorific.info@gmail.com>
2024-04-19 22:16:44 +0330

    fix key prop for OnlineUser component

--- src/components/roomAndUsers/RoomAndUsers.js
+++ src/components/roomAndUsers/RoomAndUsers.js
@@ -1,6 +1,7 @@
 import OnlineUser from '../onlineUser/OnlineUser'
 import RoomTitle from '../roomTitle/RoomTitle'
 import {StyledRoomAndUsers} from './RoomAndUsers.styles'
+import {v4 as uuidv4} from 'uuid'
 
 const RoomAndUsers = ({otherRoom, roomTitle, users}) => {
   // a prop whith value of array should be passed for list online users
@@ -9,7 +10,7 @@ const RoomAndUsers = ({otherRoom, roomTitle, users}) => {
       {otherRoom && <RoomTitle title={roomTitle} />}
       <ul className='onlineUsers'>
         {users.map((user) => {
-          return <OnlineUser {...(user)} />
+          return <OnlineUser {...user} key={uuidv4()} />
         })}
       </ul>
     </StyledRoomAndUsers>
