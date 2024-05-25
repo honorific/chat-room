522b58e4a1a10cc6e4bca8ddbe789bbccba8c4f0
honorific <honorific.info@gmail.com>
2024-05-25 12:33:15 +0330

    open  submenu with transition when clicked in username

--- src/components/onlineUserOptions/OnlineUserOptions.styles.js
+++ src/components/onlineUserOptions/OnlineUserOptions.styles.js
@@ -6,6 +6,8 @@ export const StyledOnlineUserOptions = styled.ul`
   top: calc(${(props) => props.coordinates[1]}px - ${window.scrollY}px);
   background-color: aliceblue;
   list-style-type: none;
-  display: ${(props) => (props.show ? 'block' : 'none')};
+  display: block;
+  transition: opacity 0.3s;
+  opacity: ${(props) => (props.show ? 1 : 0)};
   width: 200px;
 `
