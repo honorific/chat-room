72129e60746d5776afb45b7991d8f3004f259caf
honorific <honorific.info@gmail.com>
2024-05-28 22:23:04 +0330

    fix z-index of submenu & styling

--- src/components/onlineUserOptions/OnlineUserOptions.styles.js
+++ src/components/onlineUserOptions/OnlineUserOptions.styles.js
@@ -2,12 +2,15 @@ import styled from 'styled-components'
 
 export const StyledOnlineUserOptions = styled.ul`
   position: absolute;
-  left: calc(${(props) => props.coordinates[0]}px - 200px);
+  left: calc(${(props) => props.coordinates[0]}px - 220px);
   top: calc(${(props) => props.coordinates[1]}px - ${window.scrollY}px);
-  background-color: aliceblue;
+  background-color: black;
+  color: white;
   list-style-type: none;
   visibility: ${(props) => (props.show ? 'visible' : 'hidden')};
   transition: opacity 0.7s;
-  opacity: ${(props) => (props.show ? 1 : 0)};
+  opacity: ${(props) => (props.show ? 0.7 : 0)};
   width: 200px;
+  z-index: 99;
+  padding: 10px;
 `
