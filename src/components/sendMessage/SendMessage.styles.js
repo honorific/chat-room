ac0c5d450b363ac9dc41422cf0fa3873bb1647b8
honorific <honorific.info@gmail.com>
2024-06-07 21:55:21 +0330

    styling the sendMessage component

--- src/components/sendMessage/SendMessage.styles.js
+++ src/components/sendMessage/SendMessage.styles.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components'
 
-export const styledSendMessage = styled.form`
-    
+export const StyledSendMessage = styled.form`
+  position: absolute;
+  bottom: 0;
+  direction: rtl;
+  background-color: aliceblue;
+  display: flex;
+  width: calc(100% - 40px);
+  padding: 20px;
+  border-radius: 10px;
+  & input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+  }
+  & input:focus,
+  button:focus {
+    outline: none;
+  }
+  & button {
+    padding: 10px 20px;
+    margin-right: 20px;
+    color: white;
+    background-color: blue;
+    border: none;
+    border-radius: 5px;
+  }
 `
--- src/pages/chat/Chat.styles.js
+++ src/pages/chat/Chat.styles.js
@@ -22,5 +22,6 @@ export const StyledChat = styled.div`
   .main {
     grid-column: 4 / 13;
     direction: ltr;
+    position: relative;
   }
 `
