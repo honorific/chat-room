ef6f90b528b57b2f8fc35fdec2be91686cc48951
honorific <honorific.info@gmail.com>
2024-09-27 23:00:28 +0330

    disable moving the onlineUsers

--- src/components/onlineUser/OnlineUser.js
+++ src/components/onlineUser/OnlineUser.js
@@ -8,7 +8,7 @@ import {
 } from '../../utils/slices/general'
 import {useSelector, useDispatch} from 'react-redux'
 import {useState, useRef, useEffect} from 'react'
-import {sortByCordY} from '../../utils/slices/users'
+import {sortByCordY, sortArrayOfUsers} from '../../utils/slices/users'
 
 const OnlineUser = ({gender, username, chatting, selector}) => {
   const [cords, setCords] = useState([0, 0])
@@ -43,87 +43,151 @@ const OnlineUser = ({gender, username, chatting, selector}) => {
   }
   let oldY = 0
   const dragHandler = (e) => {
-    document.removeEventListener('click', clickHandler)
+    //document.removeEventListener('click', clickHandler)
     let listArr = [44, 88, 132, 176, 220]
-    document.querySelector(
-      '.onlineUsers',
-    ).style.height = `${listHeightRef.current}px`
-    users.forEach((u, i) => {
-      document.querySelector(
-        `.onlineUsers li:nth-child(${i + 1})`,
-      ).style.position = 'absolute'
-      document.querySelector(
-        `.onlineUsers li:nth-child(${i + 1})`,
-      ).style.width = `${elemWidth - 10}px`
-      document.querySelector(
-        `.onlineUsers li:nth-child(${i + 1})`,
-      ).style.top = `${listArr[i]}px`
-    })
+    // document.querySelector(
+    //   '.onlineUsers',
+    // ).style.height = `${listHeightRef.current}px`
+    // users.forEach((u, i) => {
+    //   document.querySelector(
+    //     `.onlineUsers li:nth-child(${i + 1})`,
+    //   ).style.position = 'absolute'
+    //   document.querySelector(
+    //     `.onlineUsers li:nth-child(${i + 1})`,
+    //   ).style.width = `${elemWidth - 10}px`
+    //   document.querySelector(
+    //     `.onlineUsers li:nth-child(${i + 1})`,
+    //   ).style.top = `${listArr[i]}px`
+    // })
 
     const mouseMoveHandler = (el) => {
       if (el.buttons === 1) {
-        elemref.current.style.position = 'absolute'
-        elemref.current.style.zIndex = '1000'
-        elemref.current.style.left = `${el.clientX}px`
-        elemref.current.style.width = `${elemWidth - 10}px`
-        elemref.current.style.boxShadow = '-9px 4px 20px 3px rgba(0, 0, 0, 0.1)'
-        users.forEach((u, i) => {
-          if (
-            el.clientY >
-              document
-                .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
-                .getBoundingClientRect().top &&
-            el.clientY > oldY &&
-            el.clientY <= 240 &&
-            el.movementY >= 1
-            // &&
-            // i + 1 ===
-            //   Math.floor(
-            //     document
-            //       .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
-            //       .getBoundingClientRect().top / 44,
-            //   )
-          ) {
-            if (i !== 0) {
-              document.querySelector(`.onlineUsers li:nth-child(${i + 1})`)
-                .style.top !== listArr[i]
-                ? (document.querySelector(
-                    `.onlineUsers li:nth-child(${i + 1})`,
-                  ).style.top = `${listArr[i]}px`)
-                : (document.querySelector(
-                    `.onlineUsers li:nth-child(${i + 1})`,
-                  ).style.top = `${listArr[i - 1]}px`)
-            } else {
-              document.querySelector(
-                `.onlineUsers li:nth-child(${i + 1})`,
-              ).style.top = `${listArr[i]}px`
-            }
+        // elemref.current.style.position = 'absolute'
+        //elemref.current.style.zIndex = '1000'
+        //elemref.current.style.left = `${el.clientX}px`
+        // elemref.current.style.width = `${elemWidth - 10}px`
+        // elemref.current.style.boxShadow = '-9px 4px 20px 3px rgba(0, 0, 0, 0.1)'
+        let elIndex
+        for (let j = 0; j < users.length; j++) {
+          if (users[j].username === elemref.current.innerText) {
+            elIndex = j
+            console.log('index of dragged elem', j)
+            break
           }
-          if (
-            el.clientY <
-              document
-                .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
-                .getBoundingClientRect().top &&
-            el.clientY < oldY &&
-            el.movementY <= -1
-          ) {
-            if (i !== 4) {
-              document.querySelector(
-                `.onlineUsers li:nth-child(${i + 1})`,
-              ).style.top =
-                document.querySelector(`.onlineUsers li:nth-child(${i + 1})`)
-                  .style.top !== listArr[i]
-                  ? `${listArr[i]}px`
-                  : `${listArr[i + 1]}px`
-            } else {
-              document.querySelector(
-                `.onlineUsers li:nth-child(${i + 1})`,
-              ).style.top = `${listArr[i]}px`
-            }
-          }
-          elemref.current.style.top = `${el.clientY}px`
-        })
-
+        }
+        // users.forEach((u, i) => {
+        //   if (
+        //     el.clientY >
+        //       document
+        //         .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
+        //         .getBoundingClientRect().top &&
+        //     el.clientY > oldY &&
+        //     el.clientY <= 240 &&
+        //     el.movementY >= 1
+        //     // &&
+        //     // i + 1 ===
+        //     //   Math.floor(
+        //     //     document
+        //     //       .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
+        //     //       .getBoundingClientRect().top / 44,
+        //     //   )
+        //   ) {
+        //     if (i !== 0) {
+        //       document
+        //         .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
+        //         .style.top.match(/\d+/)[0] != listArr[i]
+        //         ? (document.querySelector(
+        //             `.onlineUsers li:nth-child(${i + 1})`,
+        //           ).style.top = `${listArr[i]}px`)
+        //         : (document.querySelector(
+        //             `.onlineUsers li:nth-child(${i + 1})`,
+        //           ).style.top = `${listArr[i - 1]}px`)
+        //       console.log(
+        //         'top is: ',
+        //         document
+        //           .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
+        //           .style.top.match(/\d+/)[0],
+        //       )
+        //       // dispatch(sortArrayOfUsers([elIndex, i]))
+        //     } else {
+        //       document.querySelector(
+        //         `.onlineUsers li:nth-child(${i + 1})`,
+        //       ).style.top = `${listArr[i]}px`
+        //       //  dispatch(sortArrayOfUsers([elIndex, i]))
+        //     }
+        //   }
+        //   if (
+        //     el.clientY <
+        //       document
+        //         .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
+        //         .getBoundingClientRect().top &&
+        //     el.clientY < oldY &&
+        //     el.movementY <= -1
+        //   ) {
+        //     if (i !== 4) {
+        //       document.querySelector(
+        //         `.onlineUsers li:nth-child(${i + 1})`,
+        //       ).style.top =
+        //         document
+        //           .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
+        //           .style.top.match(/\d+/)[0] != listArr[i]
+        //           ? `${listArr[i]}px`
+        //           : `${listArr[i + 1]}px`
+        //       console.log(
+        //         'top is: ',
+        //         document
+        //           .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
+        //           .style.top.match(/\d+/)[0],
+        //       )
+        //       //dispatch(sortArrayOfUsers([elIndex, i]))
+        //     } else {
+        //       document.querySelector(
+        //         `.onlineUsers li:nth-child(${i + 1})`,
+        //       ).style.top = `${listArr[i]}px`
+        //       // dispatch(sortArrayOfUsers([elIndex, i]))
+        //     }
+        //   }
+        //   elemref.current.style.top = `${el.clientY}px`
+        // })
+        // let elIndex
+        // for (let j = 0; j < users.length; j++) {
+        //   if (users[j].username === elemref.current.innerText) {
+        //     elIndex = j
+        //     console.log('index of dragged elem', j)
+        //     break
+        //   }
+        // }
+        // users.forEach((u, i) => {
+        //   if (
+        //     el.clientY >
+        //     document
+        //       .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
+        //       .getBoundingClientRect().top
+        //     //     &&
+        //     // document
+        //     //   .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
+        //     //   .getBoundingClientRect().top /
+        //     //   44 ===
+        //     //   i + 1
+        //   ) {
+        //     dispatch(sortArrayOfUsers([elIndex, i]))
+        //   }
+        //   if (
+        //     el.clientY <
+        //     document
+        //       .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
+        //       .getBoundingClientRect().top
+        //     //     &&
+        //     // document
+        //     //   .querySelector(`.onlineUsers li:nth-child(${i + 1})`)
+        //     //   .getBoundingClientRect().top /
+        //     //   44 ===
+        //     //   i + 1
+        //   ) {
+        //     dispatch(sortArrayOfUsers([elIndex, i]))
+        //   }
+        // })
+        // elemref.current.style.top = `${el.clientY}px`
         oldY = el.clientY
       }
     }
