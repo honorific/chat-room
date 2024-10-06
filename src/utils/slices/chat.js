be9e77fb60e1b426ef2f7469b849b07f3122deef
honorific <honorific.info@gmail.com>
2024-10-06 23:40:28 +0330

    change dateTime in addChat reducer

--- src/utils/slices/chat.js
+++ src/utils/slices/chat.js
@@ -10,13 +10,13 @@ export const chatSlice = createSlice({
   reducers: {
     addChat: (state, action) => {
       state.chat.forEach((ch) => {
-        if (ch.name === action.payload.room) {
+        if (ch.name === action.payload.room || ch.name === undefined) {
           state.chat.value = [
             ...state.chat.value,
             {
               sender: action.payload.sender,
               receiver: action.payload.receiver,
-              DateTime: action.payload.time,
+              dateTime: action.payload.dateTime,
               msg: action.payload.msg,
             },
           ]
