91c5a91d9f4e4f81a812a5ee06ca52be2c1da761
honorific <honorific.info@gmail.com>
2024-10-08 23:07:01 +0330

    fix addChat reducer

--- src/utils/slices/chat.js
+++ src/utils/slices/chat.js
@@ -1,27 +1,45 @@
-import {createSlice} from '@reduxjs/toolkit'
+import {StarRateSharp} from '@mui/icons-material'
+import {createSlice, current} from '@reduxjs/toolkit'
 
-const initialState = {
-  chat: [],
-}
+const initialState = [
+  {
+    room: '',
+    messages: [],
+  },
+]
 
 export const chatSlice = createSlice({
   name: 'chat',
   initialState,
   reducers: {
     addChat: (state, action) => {
-      state.chat.forEach((ch) => {
-        if (ch.name === action.payload.room || ch.name === undefined) {
-          state.chat.value = [
-            ...state.chat.value,
-            {
-              sender: action.payload.sender,
-              receiver: action.payload.receiver,
-              dateTime: action.payload.dateTime,
-              msg: action.payload.msg,
-            },
-          ]
+      let newRoom = true
+      state.map((ch) => {
+        if (action.payload.room === ch.room) {
+          ch.messages.push({
+            sender: action.payload.sender,
+            receiver: action.payload.receiver,
+            dateTime: action.payload.dateTime,
+            msg: action.payload.msg,
+          })
+          newRoom = false
         }
       })
+      if (newRoom === true) {
+        return [
+          {
+            room: action.payload.room,
+            messages: [
+              {
+                sender: action.payload.sender,
+                receiver: action.payload.receiver,
+                dateTime: action.payload.dateTime,
+                msg: action.payload.msg,
+              },
+            ],
+          },
+        ]
+      }
     },
   },
 })
