91ee7499855a6036548e5cd68e1381aceacf0fdf
honorific <honorific.info@gmail.com>
2025-01-04 22:50:19 +0330

    put userApiExist in try catch and make it async

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -4,6 +4,10 @@ const userApi = axios.create({
   baseURL: 'http://localhost:4000/api/v1/user',
 })
 
-export const userApiExist = (username) => {
+export const userApiExist = async (username) => {
+  try {
     return userApi.get(`/exist?username=${username}`)
+  } catch (_err) {
+    console.log("username didn't found in db")
+  }
 }
