d298fd71778fdc84b0d35915e96057a26d2cd9be
honorific <honorific.info@gmail.com>
2025-01-04 21:14:08 +0330

    correct the name of authApi

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const userApi = axios.create({
+const authApi = axios.create({
   baseURL: 'http://localhost:4000/api/v1/auth',
 })
 
 export const authApiRegister = (args) => {
-  return userApi.post('/register', args)
+  return authApi.post('/register', args)
 }
