9644b6391cded50f410f81f3b30cbb8645e6a40d
honorific <honorific.info@gmail.com>
2025-03-29 02:07:16 +0330

    access cookie to get user token to allow deletion

--- src/api/chatApi.js
+++ src/api/chatApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import cookies from '../utils/cookies'
-// Parse the JSON string
+
 let token = null
 if (cookies.get('loggedInAs')) {
   const userData = cookies.get('loggedInAs')
@@ -9,13 +9,12 @@ if (cookies.get('loggedInAs')) {
 
 const chatApi = axios.create({
   baseURL: 'http://localhost:4000/api/v1/chat',
+  headers: {
+    Authorization: `Bearer ${token}`, // Set the Authorization header
+    'Content-Type': 'application/json', // Optional: Specify content type
+  },
 })
 
 export const chatApiAddChat = (args) => {
-  return chatApi.post('/send', args, {
-    headers: {
-      Authorization: `Bearer ${token}`, // Set the Authorization header
-      'Content-Type': 'application/json', // Optional: Specify content type
-    },
-  })
+  return chatApi.post('/send', args)
 }
