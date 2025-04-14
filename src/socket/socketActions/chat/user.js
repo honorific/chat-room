1196f7681055382d4edf7ddafbd8ae622c328a01
honorific <honorific.info@gmail.com>
2025-04-14 22:16:06 +0330

    organizing socket stuff

---
+++ src/socket/socketActions/chat/user.js
@@ -0,0 +1,16 @@
+import {resetUsers, setUsers} from '../../redux/slices/users'
+
+export const getUsersHandler = (listOfUsers, dispatch) => {
+  dispatch(resetUsers())
+  console.log('list of users are: ', listOfUsers)
+  listOfUsers.forEach((u) => {
+    dispatch(setUsers({gender: u.gender, username: u.username}))
+  })
+}
+
+export const showOnlineUsers = (emitWithSocket, onSocketEvent, dispatch) => {
+  emitWithSocket('getAllUsers')
+  onSocketEvent('getUsers', (listOfUsers) => {
+    getUsersHandler(listOfUsers, dispatch)
+  })
+}
