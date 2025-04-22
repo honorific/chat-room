3bdefd04e61aafeeb1dbf8218462706581a9ea5f
honorific <honorific.info@gmail.com>
2025-04-23 00:00:34 +0330

    remove initializing the socket after auth

--- src/socket/socketActions/chat/auth.js
+++ src/socket/socketActions/chat/auth.js
@@ -1,10 +1,9 @@
 import {startSocketListener} from '../../../socket/sockets/chatSocket'
 import {emitWithSocket} from './socketManager'
 
-export const authUser = (gender, username, store) => {
+export const authUser = (gender, username, state) => {
   // should disconnect it so that can read the new cookie
   // otherwise loggedInAs cookie is always undefined
-  startSocketListener(store)
   emitWithSocket('addUser', {gender, username})
     .then(() => {})
     .catch((err) => console.error('Failed:', err))
