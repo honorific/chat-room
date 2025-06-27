663210bf30544be31984316d5663a34852ee1ea8
honorific <honorific.info@gmail.com>
2025-06-27 18:51:27 +0330

    change authApi structure

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
 
-const authApi = axios.create({
+const authApiInstance = axios.create({
   baseURL: 'http://localhost:4000/api/v1/auth',
 })
 
-export const authApiRegister = (args) => {
-  return authApi.post('/register', args)
+export const authApiRegister = (args) => {}
+
+export const authApi = {
+  register: (args) => {
+    return authApiInstance.post('/register', args)
+  },
 }
