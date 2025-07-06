b88a9c39c668cddeaef28343323366e21cd2aa3a
honorific <honorific.info@gmail.com>
2025-07-06 14:58:53 +0330

    handle authUser with better syntax and logs

--- src/socket/socketActions/chat/auth.js
+++ src/socket/socketActions/chat/auth.js
@@ -1,13 +1,18 @@
 import {startSocketListener} from '../../../socket/sockets/chatSocket'
 import {emitWithSocket} from './socketManager'
 
-export const authUser = (gender, username, disconnect) => {
+export const authUser = (gender, username, connection) => {
   // should disconnect it so that can read the new cookie
   // otherwise loggedInAs cookie is always undefined
-  if (!disconnect) {
+  //const {initSocket} = SocketUse()
+  if (!connection) {
+    //initSocket()
     startSocketListener()
   }
   emitWithSocket('addUser', {gender, username})
-    .then(() => {})
+    .then((response) => {
+      console.log(`user Added with authUser with: ${gender} | ${username}`)
+      console.log(response)
+    })
     .catch((err) => console.error('Failed:', err))
 }
