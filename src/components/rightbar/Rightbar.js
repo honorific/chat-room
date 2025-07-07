1fd45535cb46cf59224b6b29f738b2ea35fdb93b
honorific <honorific.info@gmail.com>
2025-07-07 18:08:14 +0330

    handling chatSocket for rightbar.js

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -3,7 +3,7 @@ import RoomAndUsers from '../roomAndUsers/RoomAndUsers'
 import {StyledRightbar} from './Rightbar.styles'
 import {resetChatOpen} from '../../redux/slices/general'
 import {useEffect, useState} from 'react'
-import {chatSocket} from '../../socket/sockets/chatSocket'
+import {getChatSocket} from '../../socket/sockets/chatSocket'
 import cookies from '../../utils/cookie/initialize'
 import {loginUser, setLoggedInAs} from '../../redux/slices/users'
 import {showOnlineUsers} from '../../socket/socketActions/chat/user'
@@ -21,6 +21,7 @@ const Rightbar = () => {
     (state) => state.rootReducer.chatSocketConnection,
   )
   const dispatch = useDispatch()
+  const chatSocket = getChatSocket()
   const scrollHandler = () => {
     dispatch(resetChatOpen())
   }
@@ -37,27 +38,64 @@ const Rightbar = () => {
         console.log("couldn't retrieve user from db")
       }
     }
+    // const initialize = async () => {
+    //   if (cookies.get('loggedInAs')) {
+    //     const {gender, username} = cookies.get('loggedInAs')
+    //     console.log('gender of cookie:', gender)
+    //     console.log('username of cookie:', username)
+    //     try {
+    //       // 1. Wait for socket initialization AND connection
+    //       const connectedSocket = await initSocket()
+    //       console.log('connectedSocket is: ', connectedSocket)
+    //       dispatch(setLoggedInAs(cookies.get('loggedInAs')))
+    //       //authUser(gender, username, chatSocketConnection)
+    //       //initSocket()
+    //       safeEmit('addUser', {gender, username}).then((response) => {
+    //         console.log('response of addUser: ', response)
+    //       })
+    //       dispatch(connectChatSocketConnection())
+    //       usernameChecker(username)
+    //     } catch (error) {
+    //       console.error('Initialization failed:', error)
+    //     }
+    //   }
+    // }
+    // initialize()
     if (cookies.get('loggedInAs')) {
       const {gender, username} = cookies.get('loggedInAs')
       console.log('gender of cookie:', gender)
-      console.log('username of cookie:', username)
       // cleanupSocket()
       console.log('username of cookie is: ', username)
       dispatch(setLoggedInAs(cookies.get('loggedInAs')))
       authUser(gender, username, chatSocketConnection)
+      //initSocket()
       dispatch(connectChatSocketConnection())
       usernameChecker(username)
     }
   }, [])
   useEffect(() => {
-    if (chatSocket.connected) {
-      if (!chatSocketConnection) {
-        dispatch(connectChatSocketConnection())
-      }
-      showOnlineUsers(dispatch)
-    } else {
-      dispatch(disconnectChatSocketConnection())
-    }
+    // if (chatSocket.connected) {
+    //   if (!chatSocketConnection) {
+    //     dispatch(connectChatSocketConnection())
+    //   }
+
+    //   console.log('rightbar tried to showOnlineUsers')
+    // } else {
+    //   dispatch(disconnectChatSocketConnection())
+    // }
+    console.log('chatSocket in rightbar', chatSocket)
+    showOnlineUsers(dispatch)
+    // safeEmit('getAllUsers', {})
+    //   .then((response) => {
+    //     console.log('Received users in getAllUsers of rightbar:', response)
+    //     dispatch(resetUsers())
+    //     response.forEach((u) => {
+    //       dispatch(setUsers({gender: u.gender, username: u.username}))
+    //     })
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error of getAllUsers:', error)
+    //   })
 
     // cant send request of user deletion from database in client, because
     // it wont affect the last user that is online
